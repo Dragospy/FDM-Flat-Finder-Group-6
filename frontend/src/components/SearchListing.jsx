17855d7d18b41bb800441c9fe60eff11eb4c5571
kb <ec24700@qmul.ac.uk>
2026-04-05 10:57:45 +0100

    Adding display search result functionality. - Only partially implemented - Currently only displays one of the listing results.

--- frontend/src/components/SearchListing.jsx
+++ frontend/src/components/SearchListing.jsx
@@ -14,6 +14,61 @@ import { getListings, getListing, getListingsByHost } from "../lib/api.js";
 
 //      location : Will require lat+lon to compare distance?
 
+function DisplayImages({images}){
+    const listImages = images.map(image => <img src={image} key={image} />);
+
+    return <div>{listImages}</div>
+}
+
+function DisplayAmenities({amenities}){
+    const listingAmenities = amenities.map(amenity => <p key={amenity}>{amenity}</p>);
+
+    return <ul>{listingAmenities}</ul>
+}
+
+function DisplayListing({listing}){
+    return(
+        <>
+            <div className="listing">
+                <h2>{listing.title}</h2>
+                    <h3>Details:</h3>
+                        <div>
+                            <p>Rating: {listing.rating}</p>
+                            <p>Review count: {listing.reviewCount}</p>
+                        </div>
+                        <p>Price: {listing.price} per {listing.priceUnit}</p>
+                        <p>Type: {listing.type}</p>
+                        <p>Bedrooms:{listing.bedrooms}</p>
+                        <p>Bathrooms:{listing.bathrooms}</p>
+                        <p>Maximum number of guests: {listing.maxGuests}</p>
+
+                        <h4>Amenities:</h4>
+                            <DisplayAmenities amenities={listing.amenities}/>
+                    <h3>Description:</h3>
+                        <p>{listing.description}</p>
+                    <h3>Images:</h3>
+                        <DisplayImages images={listing.images}/>
+                    <h3>Location:</h3>       
+                        <p>Postcode: {listing.location.postcode}</p>
+                        <p>Address: {listing.location.address}</p>
+                        <p>City: {listing.location.city}</p>
+                        <p>Country: {listing.location.country}</p>             
+            </div>
+        
+        </>
+    )
+
+}
+
+// function DisplayListings({listings}){
+//     let listOfListing = [];
+//     listOfListing.push(DisplayListing(listings[0]));
+
+
+//     const displayListings = listings.map(listing => );
+
+//     return <ul>{displayListings}</ul>
+// }
 
 export function SearchListings() {
     let ascending = true;
@@ -38,7 +93,7 @@ export function SearchListings() {
 
     return (
         <div>
-            <p>Test!</p>
+            <DisplayListing listing={listings[0]}/>
         </div>
 
         
